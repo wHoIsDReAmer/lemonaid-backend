5e90c4d6a16ac32125163d903d71d09cf96f930c
wHoIsDReAmer <lcw060403@gmail.com>
2023-11-09 13:43:31 +0900

    fix: search permission

--- routes/v1/search.go
+++ routes/v1/search.go
@@ -20,19 +20,19 @@ import (
 */
 
 func SearchPostAndTeachers(c *fiber.Ctx) error {
-	email := c.Locals("email")
+	//email := c.Locals("email")
 	name := c.Query("value")
-	
-	var user db.User
-	db.DB.Select("plan").Where("email = ?", email).Find(&user)
 
-	if user.Plan != db.RESUME && user.Plan != db.SPECIALIST {
-		return c.JSON(fiber.Map{
-			"status":  fiber.StatusForbidden,
-			"message": "Permission denied",
-		})
-	}
-	
+	//var user db.User
+	//db.DB.Select("plan").Where("email = ?", email).Find(&user)
+	//
+	//if user.Plan != db.RESUME && user.Plan != db.SPECIALIST {
+	//	return c.JSON(fiber.Map{
+	//		"status":  fiber.StatusForbidden,
+	//		"message": "Permission denied",
+	//	})
+	//}
+
 	if len(strings.Replace(name, " ", "", -1)) == 0 {
 		return c.JSON(fiber.Map{
 			"status":   200,
@@ -46,13 +46,13 @@ func SearchPostAndTeachers(c *fiber.Ctx) error {
 
 	db.DB.
 		Select("id, first_name, last_name, email, phone_number, birthday, gender, nationality, image").
-		Where("user_type = ? and (first_name like ? or last_name like ?)", "2", "%" + name + "%", "%" + name + "%").
+		Where("user_type = ? and (first_name like ? or last_name like ?)", "2", "%"+name+"%", "%"+name+"%").
 		Find(&teachers)
 
 	db.DB.
-		Where("academy like ? or campus like ?", "%" + name + "%", "%" + name + "%").
-		Find(&posts)	
-	
+		Where("academy like ? or campus like ?", "%"+name+"%", "%"+name+"%").
+		Find(&posts)
+
 	return c.JSON(fiber.Map{
 		"status":   200,
 		"teachers": teachers,
