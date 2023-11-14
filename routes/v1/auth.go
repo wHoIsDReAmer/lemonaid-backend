bcab7b5d0348260e00442c620c8f6c7db7dafa19
wHoIsDReAmer <lcw060403@gmail.com>
2023-11-14 16:37:43 +0900

    test: oauth function

--- routes/v1/auth.go
+++ routes/v1/auth.go
@@ -138,6 +138,8 @@ func Register(c *fiber.Ctx) error {
 			Find(&sess)
 	}
 
+	fmt.Printf("OAuthing: %d", sess.OAuthing)
+
 	if !emailValidation(email[0]) && sess.OAuthing != 1 {
 		return c.JSON(fiber.Map{
 			"status":  fiber.StatusBadRequest,
